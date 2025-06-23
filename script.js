9df763538c97bd3919f6b76d3393ce6ea47b73d7
fatahfatah568 <abdelfettahmisbah1234@gmail.com>
2025-06-24 00:56:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("submit-btn").addEventListener("click", function () {
   sendToChatGPT();
 });
 
-// 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
+// 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
 
 function sendToChatGPT() {
   let value = document.getElementById("word-input").value;
